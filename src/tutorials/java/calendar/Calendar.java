f94602411237ab68ab718ac77b8b9a207182edba
admin <admin@192.168.123.103>
2020-10-01 21:09:14 +0900

    Find final day of month using method

--- src/tutorials/java/calendar/Calendar.java
+++ src/tutorials/java/calendar/Calendar.java
@@ -3,7 +3,12 @@ package tutorials.java.calendar;
 import java.util.Scanner;
 
 public class Calendar {
-
+	
+	public final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	public int getMaxDaysOfMonth(int month) {
+		return MAX_DAYS[month-1];
+	}
+	
 	public static void main(String[] args) {
 		System.out.println(" 일 월 화 수 목 금 토");
 		System.out.println("--------------------");
@@ -12,12 +17,13 @@ public class Calendar {
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
 		
+		Calendar cal = new Calendar();
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("달을 입력하세요.");
-		int a = scanner.nextInt();
-		int[] days = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-		int b = a-1;
-		System.out.printf("%d월은 %d일까지 있습니다.", a, days[b]);
+		int month = scanner.nextInt();
+		
+		System.out.printf("%d월은 %d일까지 있습니다.", month, cal.getMaxDaysOfMonth(month));
+		
 		
 		
 		
