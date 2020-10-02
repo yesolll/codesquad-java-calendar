17ada276a091dbc13a150bbc13ce5abdb864610b
admin <admin@192.168.123.103>
2020-10-02 19:06:02 +0900

    Print Calendar(1)

--- src/tutorials/java/calendar/Calendar.java
+++ src/tutorials/java/calendar/Calendar.java
@@ -1,34 +1,32 @@
 package tutorials.java.calendar;
 
-import java.util.Scanner;
-
 public class Calendar {
 
 	public final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
 	public int getMaxDaysOfMonth(int month) {
-		return MAX_DAYS[month-1];
+		return MAX_DAYS[month - 1];
 	}
 
-	public void pirntSampleCalendar() {
-		System.out.println(" 일 월 화 수 목 금 토");
-		System.out.println("--------------------");
-		System.out.println(" 1  2  3  4  5  6  7");
-		System.out.println(" 8  9 10 11 12 13 14");
-		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 26 27 28");
-	}
+	public void printCalendar(int year, int month) {
+		System.out.printf("    <<%4d년%3d월>>\n", year, month);
+		System.out.println(" SU MO TU WE TH FR SA");
+		System.out.println("---------------------");
 
-	public static void main(String[] args) {
-		Calendar cal = new Calendar();
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("달을 입력하세요.");
-		int month = scanner.nextInt();
+		int maxDay = getMaxDaysOfMonth(month);
 
-		System.out.printf("%d월은 %d일까지 있습니다.\n\n", month, cal.getMaxDaysOfMonth(month));
-		cal.pirntSampleCalendar();
-		scanner.close();
-
+		for (int i = 1; i <= maxDay; i++) {
+			System.out.printf("%3d", i);
+			if (i % 7 == 0) {
+				System.out.println();
+			}
+//			System.out.println();
+//			System.out.println(" 1  2  3  4  5  6  7");
+//			System.out.println(" 8  9 10 11 12 13 14");
+//			System.out.println("15 16 17 18 19 20 21");
+//			System.out.println("22 23 24 25 26 27 28");
+//			System.out.println("");
+		}
 	}
 
 }
--- src/tutorials/java/calendar/CalendarMain.java
+++ src/tutorials/java/calendar/CalendarMain.java
@@ -3,24 +3,31 @@ package tutorials.java.calendar;
 import java.util.Scanner;
 
 public class CalendarMain {
-	public static void main(String[] args) {
+
+	public void runCalendarMain() {
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
 
 		try {
-			
-		int month = 1;
-		while (month != -1) {
-			System.out.println("달을 입력하세요.");
-			System.out.print(">");
-			month = scanner.nextInt();
-			System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
-		}
-		}catch (Exception e){
+
+			int month = 1;
+			while (month != -1) {
+				System.out.println("달을 입력하세요.");
+				System.out.print(">");
+				month = scanner.nextInt();
+				System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
+				cal.printCalendar(2020, month);
+			}
+		} catch (Exception e) {
 			System.out.println("1~12의 값을 입력하세요.");
 			System.out.println("프로그램을 종료합니다.");
 		}
 		scanner.close();
 	}
 
+	public static void main(String[] args) {
+		CalendarMain cm = new CalendarMain();
+		cm.runCalendarMain();
+		
+	}
 }
